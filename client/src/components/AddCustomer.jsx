fb9554bb7d80a7f391a89f596ff903e30e4d67f1
Ariel-Joe <arieljoe00@gmail.com>
2025-01-17 19:58:42 +0300

    modal update

--- client/src/components/AddCustomer.jsx
+++ client/src/components/AddCustomer.jsx
@@ -2,13 +2,41 @@ import React from "react";
 
 const AddCustomer = () => {
   return (
-    <div className="items-center justify-center w-full md:w-full p-4">
-      <form className="bg-white px-8 py-4 rounded-lg w-full max-w-3xl">
-        <div>
-          <div className="flex">
-            <label>Name</label>
-          <input type="text" className="border w-full" />
+    <div className="items-center justify-center w-full md:w-[560px] p-2">
+      <form className="bg-white px-8 py-4 rounded-lg w-full">
+        <div className="flex flex-col items-center">
+          <div>
+            <h1 className="font-semibold text-xl mb-5">New Customer</h1>
+          </div>
 
+          {/*  */}
+          <div className="flex items-center mb-3 w-full">
+            <label className="text-lg">Name:</label>
+            <input
+              type="text"
+              className="border w-full rounded-md ml-2 py-2 px-3"
+              placeholder="John Doe"
+            />
+          </div>
+
+          {/*  */}
+          <div className="flex items-center mb-3 w-full">
+            <label className="text-lg">Phone:</label>
+            <input
+              type="text"
+              className="border w-full rounded-md ml-2 py-2 px-3"
+              placeholder="+254712345678"
+            />
+          </div>
+
+          {/*  */}
+          <div className="flex items-center mb-3 w-full">
+            <label className="text-lg">No plate:</label>
+            <input
+              type="text"
+              className="border w-full rounded-md ml-2 py-2 px-3"
+              placeholder="KAA 001A"
+            />
           </div>
         </div>
       </form>
--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { useEffect, useRef } from "react";
+import { SlClose } from "react-icons/sl";
+
 
 const Modal = ({ openModal, closeModal, children }) => {
   const ref = useRef();
@@ -15,7 +17,13 @@ const Modal = ({ openModal, closeModal, children }) => {
     <div>
       <dialog ref={ref} onCancel={closeModal}>
         {children}
-        <button onClick={closeModal}>Close</button>
+        <button
+          onClick={closeModal}
+          className="border border-black flex items-center text-black px-4 py-2 rounded-md hover:cursor-pointer mb-6 ml-6"
+        >
+            <SlClose size={20} className="mr-1" />
+          Cancel
+        </button>
       </dialog>
     </div>
   );
