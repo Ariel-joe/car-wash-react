79f5ec1da9185581fd659b8c603f490dd9e2c3d2
Ariel-Joe <arieljoe00@gmail.com>
2025-02-09 15:42:20 +0300

    updating controllers path

---
+++ server/src/controllers/vehicleTypes.js
@@ -0,0 +1,24 @@
+import { Vehicle_type } from "../database/models/vehicleType.js";
+
+export const vehicleTypefunc = async (req, res) => {
+  try {
+    const { status, type } = req.body;
+    const vehicleTypeData = {
+      status,
+      type,
+    };
+    const typeData = await Vehicle_type.create(vehicleTypeData);
+
+    return res.status(201).json({
+      success: true,
+      data: typeData,
+    });
+  } catch (error) {
+    console.error(error.message);
+
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
--- server/src/routes/vehiclesRoute.js
+++ server/src/routes/vehiclesRoute.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
 import { addVehicles, getVehicles } from "../controllers/vehicles.js";
+import { vehicleTypefunc } from "../controllers/vehicleTypes.js";
 
 const vehiclesRoute = Router();
 
-vehiclesRoute.route("/vehicles").get(getVehicles).post(addVehicles)
+vehiclesRoute.get("/vehicles", getVehicles);
 
+vehiclesRoute.post("/vehicles/add", addVehicles);
+vehiclesRoute.post("/vehicles/types/add", vehicleTypefunc);
 
 export { vehiclesRoute };
