602df6a02b3e49c9227011e2f4ee2ca3e3f22bd6
Ariel-Joe <arieljoe00@gmail.com>
2025-02-22 15:31:38 +0300

    customer updated

--- server/src/controllers/customers.js
+++ server/src/controllers/customers.js
@@ -1,27 +1,80 @@
 import { Customer } from "../database/models/customer.js";
+import { Detailer } from "../database/models/detailer.js";
+import { Service } from "../database/models/service.js";
+import { Vehicle } from "../database/models/vehicle.js";
+import { VehicleType } from "../database/models/vehicleType.js";
 
 // adding a customer
 export const addCustomer = async (req, res) => {
   try {
-    const { name, phone } = req.body;
+    const {
+      name,
+      phone,
+      numberPlate,
+      vehicleType,
+      service,
+      amount,
+      detailer,
+    } = req.body;
+
+    
+
+    // Validate and fetch the vehicleType, service, and detailer from the database
+    const vehicleTypeData = await VehicleType.findOne({type: vehicleType});
+    if (!vehicleTypeData) {
+      return res.status(404).json({
+        success: false,
+        message: "Vehicle type not found",
+      });
+    }
+
+    const serviceData = await Service.findOne({service: service});
+    if (!serviceData) {
+      return res.status(404).json({
+        success: false,
+        message: "Service not found",
+      });
+    }
+
+    const detailerData = await Detailer.findOne({name: detailer});
+    if (!detailerData) {
+      return res.status(404).json({
+        success: false,
+        message: "Detailer not found",
+      });
+    }
 
     const customerData = {
       name,
       phone,
+      numberPlate,
+      vehicleType: vehicleTypeData._id,
+      service: serviceData._id,
+      amount,
+      detailer: detailerData._id,
     };
 
     const newCustomer = await Customer.create(customerData);
 
+    const vehicleData = {
+      customer: newCustomer._id,
+      vehicle_type: vehicleTypeData._id,
+      number_plate: numberPlate,
+      status: "Pending",
+    }
+
+    const newVehicle = await Vehicle.create(vehicleData);
+
     return res.status(201).json({
       success: true,
-      newCustomer: newCustomer,
+      newCustomer: newCustomer, newVehicle
     });
   } catch (error) {
     console.log(error);
 
     return res.json({
       success: false,
-      message: "failed to add customer",
+      message: error.message,
     });
   }
 };
@@ -50,21 +103,18 @@ export const findCustomer = async (req, res) => {
   try {
     const customerId = req.query.id;
 
-    const customer = await Customer.findOne(
-      { _id: customerId },
-      req.body
-    )
+    const customer = await Customer.findOne({ _id: customerId }, req.body);
 
     return res.status(200).json({
       success: true,
       customer: customer,
     });
   } catch (error) {
-    console.log(error);
+    console.log(error.message);
 
     return res.json({
       success: false,
-      message: "failed to fetch the customer",
+      message: error.message,
     });
   }
 };
--- server/src/database/models/customer.js
+++ server/src/database/models/customer.js
@@ -4,6 +4,11 @@ const customerSchema = new Schema(
   {
     name: { type: String, required: true },
     phone: { type: String, required: true },
+    numberPlate: { type: String, required: true },
+    vehicleType: { type: Schema.Types.ObjectId, ref: "VehicleType" },
+    detailer: { type: Schema.Types.ObjectId, ref: "Detailer" },
+    service: { type: Schema.Types.ObjectId, ref: "Service" },
+    amount: { type: String, required: true },
   },
   {
     timestamps: true,
