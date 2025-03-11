9abae52c7d1499b3f4a4a9e2bbdc66f944fd353c
Ariel-Joe <arieljoe00@gmail.com>
2025-03-11 13:50:45 +0300

    zustand implement

--- client/src/pages/CompServices.jsx
+++ client/src/pages/CompServices.jsx
@@ -5,6 +5,7 @@ import React, { useEffect, useState } from "react";
 const CompServices = () => {
   const [data, setData] = useState([]);
   const { fetchVehicles, vehicles, isLoading } = useVehicleStore();
+  const [vehiclesCompleted, setVehiclesCompleted] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -18,8 +19,12 @@ const CompServices = () => {
     setData(vehicles);
   }, [vehicles]);
 
-
-
+  // filtering
+  useEffect(() => {
+    setVehiclesCompleted(
+      vehicles.filter((vehicle) => vehicle.status === "Completed")
+    );
+  }, [vehicles]);
 
   return (
     <>
@@ -62,26 +67,28 @@ const CompServices = () => {
                   <Spin size="large" />
                 </td>
               </tr>
+            ) : vehiclesCompleted.length === 0 ? (
+              <tr>
+                <td colSpan="8" className="h-screen text-center py-4">
+                  No vehicles yet!
+                </td>
+              </tr>
             ) : (
-              data
-                .filter((elem) => elem.status === "Completed")
-                .map((elem, i) => (
-                  <tr key={i} className="border-b-2">
-                    <th className="py-2 font-light">{elem.customer.name}</th>
-                    <th className="py-2 font-light">{elem.customer.phone}</th>
-                    <th className="py-2 font-light">
-                      {elem.vehicle_type.type}
-                    </th>
-                    <th className="py-2 font-light">{elem.number_plate}</th>
-                    <th className="py-2 font-light">{elem.service.service}</th>
-                    <th className="py-2 font-light">
-                      ksh. {elem.customer.amount}
-                    </th>
-                    <th className="py-2 font-light">{elem.detailer.name}</th>
-                    <th className="py-2 font-light">{elem.status}</th>
-                    <th className="py-2 font-light"></th>
-                  </tr>
-                ))
+              vehiclesCompleted.map((elem, i) => (
+                <tr key={i} className="border-b-2">
+                  <th className="py-2 font-light">{elem.customer.name}</th>
+                  <th className="py-2 font-light">{elem.customer.phone}</th>
+                  <th className="py-2 font-light">{elem.vehicle_type.type}</th>
+                  <th className="py-2 font-light">{elem.number_plate}</th>
+                  <th className="py-2 font-light">{elem.service.service}</th>
+                  <th className="py-2 font-light">
+                    ksh. {elem.customer.amount}
+                  </th>
+                  <th className="py-2 font-light">{elem.detailer.name}</th>
+                  <th className="py-2 font-light">{elem.status}</th>
+                  <th className="py-2 font-light"></th>
+                </tr>
+              ))
             )}
           </tbody>
         </table>
